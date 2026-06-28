27484e52807fb43cee618ece32ceafabcc396924
d44niilsolyanoy <d4niilsolyaoi@gmail.com>
2026-06-28 18:57:14 +0300

    README.md

--- sctrademanager/wsgi.py
+++ sctrademanager/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for p2p_project project.
+WSGI config for sctrademanager project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'p2p_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sctrademanager.settings')
 
 application = get_wsgi_application()
